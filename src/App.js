2ac23542ee155c9cd4aa6f5ec1b8b266362d6f89
Anuna29 <anusukh29@gmail.com>
2024-11-04 14:50:05 -0600

    defined the paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 export const App = () => {
   return (
-    <div>App</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>}/>
+          <Route path="/blogs" element={<div>All Blogs Page</div>}/>
+          <Route path="/blogs/:id" element={<div>Single Blog Page</div>}/>
+          <Route path="/contact-us" element={<div>Contact Us</div>}/>
+          <Route path="/404" element={<div>Not Found Page</div>}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
